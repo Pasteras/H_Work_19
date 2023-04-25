da6703c57a5d181a5cdde23427904183821ad71b
Pasteras <128259268+Pasteras@users.noreply.github.com>
2023-04-25 11:43:28 +0300

    Update H_Work_18_Task2.cpp

--- Task2/H_Work_18_Task2.cpp
+++ Task2/H_Work_18_Task2.cpp
@@ -6,24 +6,22 @@
 
 using namespace std;
 
-void Aref(int arr[], int SIZE, int sum, int i);
+int Aref(int arr[], int SIZE, int sum, int i);
 
 int main()
 {
     const int SIZE = 10;
     int arr[SIZE]{ 8, 5, 3, 77, 32, 23, 65, 23, 1, 65 };
     Aref(arr, SIZE, 0, 0);
+    cout << "Arithmetic mean: " << Aref(arr, SIZE, 0, 0) << endl;
 }
 
-void Aref(int arr[], int SIZE, int sum, int i)
+int Aref(int arr[], int SIZE, int sum, int i)
 {
     if(i == SIZE)
     {
-        sum /= SIZE;
-        cout << "Arithmetic mean: " << sum << endl;
+        return sum /= SIZE;
     }
     sum += arr[i];
-    i++;
-    Aref(arr, SIZE, sum, i);
+    Aref(arr, SIZE, sum, ++i);
 }
-
